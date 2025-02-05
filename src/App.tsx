0cde63e4538dae39723014377f4fc589e37e2e63
Ilya-Hlebik <felix.2894@mail.ru>
2025-02-04 22:45:50 -0600

    Update libs version. Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import {Button} from "@chakra-ui/react"
+import {Grid, GridItem, Show} from "@chakra-ui/react"
+import React from "react";
 
 function App() {
 
     return (
-        <Button>Click me</Button>
+        <Grid templateAreas={{
+            base: `"nav" "main"`,
+            lg: `"nav nav" "aside main"`
+        }}>
+            <GridItem area={'nav'} bg={'coral'}>Nav</GridItem>
+            <Show above={'lg'}>
+                <GridItem area={'aside'} bg={'gold'}>Aside</GridItem>
+            </Show>
+            <GridItem area={'main'} bg={'dodgerblue'}>Main</GridItem>
+
+        </Grid>
+
     )
 }
 
