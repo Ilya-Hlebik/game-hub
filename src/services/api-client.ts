8db86b789f541f17a95604ecef2921e0f772152b
Ilya-Hlebik <felix.2894@mail.ru>
2025-03-10 22:26:00 -0500

    Refactoring fetch games/genres/platforms using react query

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+    count: number;
+    results: T[];
+}
+
 export default axios.create({
-    baseURL:'https://api.rawg.io/api',
-    params:{
-        key:'284e66a14b9c4dfd8976794acfdd4d11'
+    baseURL: 'https://api.rawg.io/api',
+    params: {
+        key: '284e66a14b9c4dfd8976794acfdd4d11'
     }
 })
